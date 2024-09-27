da9c5be0243dbf9326edd285e38b6efbd20b3537
‎ ‎ ‎ ‎ ‎ ‎ ‎👽Harsh Singh👽 <harshjuly12@gmail.com>
2024-09-27 22:51:57 +0530

    Delete family.pl

--- family.pl
+++
@@ -1,6 +0,0 @@
-%family.pl
-parent(harsh, john).    % harsh is the parent of john
-parent(john, sydney).   % john is the parent of sydney
-parent(henry, cavil).
-
-grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
